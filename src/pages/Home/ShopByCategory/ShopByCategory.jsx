ff429bb01fa0c07431a18a7f4ca344f4327dd41c
robiul-romjan <robiulromjan7@gmail.com>
2023-05-19 19:28:50 +0600

    category section done

--- src/pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 
 
 const ShopByCategory = () => {
-    const [category, setCategory] = useState('')
-    console.log(category)
-    const handleTeddy = (data) => {
-        setCategory(data)
+    const [toys, setToys] = useState([])
+    const [activeTab, setActiveTab] = useState('teddyBear')
+
+    // console.log(toys)
+
+    useEffect(() => {
+        fetch("http://localhost:5000/allToys")
+            .then(res => res.json())
+            .then(data => setToys(data))
+    }, [])
+
+    const filterByCategory = toys.filter(toy => toy.category == activeTab) || "Not data";
+
+
+    const handleSubCategory = (data) => {
+        setActiveTab(data)
     }
     return (
         <div className="container mx-auto mt-5">
@@ -16,19 +30,94 @@ const ShopByCategory = () => {
             <div>
                 <Tabs>
                     <TabList>
-                        <Tab onClick={()=>handleTeddy("Teddy")}>Teddy Bear</Tab>
-                        <Tab onClick={()=>handleTeddy("Horse")}>Horse</Tab>
-                        <Tab onClick={()=>handleTeddy("Cats")}>Cats</Tab>
+                        <Tab onClick={() => handleSubCategory("teddyBear")}>Teddy Bear</Tab>
+                        <Tab onClick={() => handleSubCategory("horse")}>Horse</Tab>
+                        <Tab onClick={() => handleSubCategory("cat")}>Cats</Tab>
                     </TabList>
 
                     <TabPanel>
-                        <h2>{category} Bear category Toys</h2>
+                        <div className='row'>
+                            {
+                                filterByCategory.map(item => <div key={item._id} className='col-lg-6'>
+                                    <div className="card mb-3 p-3">
+                                        <div className="row g-0 d-flex align-items-center">
+                                            <div className="col-lg-7">
+                                                <img src={item.photo} className=" rounded-start category-image" alt="..." />
+                                            </div>
+                                            <div className="col-lg-5">
+                                                <div className="card-body">
+                                                    <h4>{item.name}</h4>
+                                                    <div><span className='fs-2 fw-bold text-danger'>${item.price}</span></div>
+                                                    <div className='d-flex align-items-center my-3'>
+                                                        <span className='p-2 rounded-circle bg-warning fw-bold text-white'>{item.rating}</span>
+                                                        <span className='ms-3'>
+                                                            <Rating style={{ maxWidth: 100 }} value={item.rating} readOnly />
+                                                        </span>
+                                                    </div>
+                                                    <button className='btn-all'>View Detail</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>)
+                            }
+                        </div>
                     </TabPanel>
                     <TabPanel>
-                        <h2>{category} category Toys</h2>
+                        <div className='row'>
+                            {
+                                filterByCategory.map(item => <div key={item._id} className='col-lg-6'>
+                                    <div className="card mb-3 p-3">
+                                        <div className="row g-0 d-flex align-items-center">
+                                            <div className="col-lg-7">
+                                                <img src={item.photo} className=" rounded-start category-image" alt="..." />
+                                            </div>
+                                            <div className="col-lg-5">
+                                                <div className="card-body">
+                                                    <h4>{item.name}</h4>
+                                                    <div><span className='fs-2 fw-bold text-danger'>${item.price}</span></div>
+                                                    <div className='d-flex align-items-center my-3'>
+                                                        <span className='p-2 rounded-circle bg-warning fw-bold text-white'>{item.rating}</span>
+                                                        <span className='ms-3'>
+                                                            <Rating style={{ maxWidth: 100 }} value={item.rating} readOnly />
+                                                        </span>
+                                                    </div>
+                                                    <button className='btn-all'>View Detail</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>)
+                            }
+                        </div>
                     </TabPanel>
                     <TabPanel>
-                    <h2>{category} category Toys</h2>
+                        <div className='row'>
+                            {
+                                filterByCategory.map(item => <div key={item._id} className='col-lg-6'>
+                                    <div className="card mb-3 p-3">
+                                        <div className="row g-0 d-flex align-items-center">
+                                            <div className="col-lg-7">
+                                                <img src={item.photo} className=" rounded-start category-image" alt="..." />
+                                            </div>
+                                            <div className="col-lg-5">
+                                                <div className="card-body">
+                                                    <h4>{item.name}</h4>
+                                                    <div><span className='fs-2 fw-bold text-danger'>${item.price}</span></div>
+                                                    <div className='d-flex align-items-center my-3'>
+                                                        <span className='p-2 rounded-circle bg-warning fw-bold text-white'>{item.rating}</span>
+                                                        <span className='ms-3'>
+                                                            <Rating style={{ maxWidth: 100 }} value={item.rating} readOnly />
+                                                        </span>
+                                                    </div>
+                                                    <button className='btn-all'>View Detail</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>)
+                            }
+                        </div>
                     </TabPanel>
                 </Tabs>
             </div>
